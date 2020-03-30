7a73e41c4c4dcc7b2dc42b1ea572573129cc7583
Ali Bulut <alibulut@yahoo.com>
2020-03-30 18:32:59 +0300

    adding userslist and useritem components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
+    const USERS = [
+        {
+            id:'u1',
+            name:'Ali Bulut',
+            image:'https://avatars2.githubusercontent.com/u/53226554?s=460&u=5608076f8a8d017db5345a223a9a6234d5c50975&v=4',
+            places:3
+        }
+    ];
     return (
         <div>
-            <h2>Users Works!</h2>
+            <UsersList items={USERS}/>
         </div>
     );
 };
