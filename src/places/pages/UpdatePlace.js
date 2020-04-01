8421fe234cc48394979d9848be2415d955c7b93c
Ali Bulut <alibulut@yahoo.com>
2020-04-01 16:12:47 +0300

    creating a custom form hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,55 +1,135 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
-import Input from '../../shared/components/FormElements/Input';
-import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
 
-import './PlaceForm.css'
+import "./PlaceForm.css";
 
-const DUMMY_PLACES=[
-    {
-        id:'p1',
-        title:'Maiden Tower',
-        description:'One of the most famous place in the world!',
-        imageUrl:'https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG',
-        address:'Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul',
-        location:{
-            lat: 41.0211216,
-            lng: 29.0041105
-        },
-        creator:'u1'
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Maiden Tower",
+    description: "One of the most famous place in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG",
+    address: "Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul",
+    location: {
+      lat: 41.0211216,
+      lng: 29.0041105
     },
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Maiden Tower",
+    description: "One of the most famous place in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG",
+    address: "Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul",
+    location: {
+      lat: 41.0211216,
+      lng: 29.0041105
+    },
+    creator: "u2"
+  }
+];
+
+const UpdatePlace = props => {
+  const [isLoading, setIsLoading] = useState(true);
+  const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
     {
-        id:'p2',
-        title:'Maiden Tower',
-        description:'One of the most famous place in the world!',
-        imageUrl:'https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG',
-        address:'Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul',
-        location:{
-            lat: 41.0211216,
-            lng: 29.0041105
+      title: {
+        value: "",
+        isValid: false
+      },
+      description: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  useEffect(() => {
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true
+          }
         },
-        creator:'u2'
+        true
+      );
     }
-]
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
 
-const UpdatePlace = (props) => {
-    const placeId=useParams().placeId; 
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
-    const identifiedPlace=DUMMY_PLACES.find(p=>p.id === placeId);
-
-    if(!identifiedPlace){ 
-        return <div className="center"><h2>Could not find place!</h2></div>
-    }
-
+  if (!identifiedPlace) {
     return (
-        <form className="place-form">
-            <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title!" onInput={() => {}} value={identifiedPlace.title} valid={true} />
-            <Input id="description" element="textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description! (at least 5 characters)" onInput={() => {}} value={identifiedPlace.description} valid={true} />
-            <Button type="submit" disabled={true}>Update Place</Button>
-
-        </form>
+      <div className="center">
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
+      </div>
     );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title!"
+        onInput={inputHandler}
+        value={formState.inputs.title.value}
+        valid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description! (at least 5 characters)"
+        onInput={inputHandler}
+        value={formState.inputs.description.value}
+        valid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        Update Place
+      </Button>
+    </form>
+  );
 };
 
 export default UpdatePlace;
