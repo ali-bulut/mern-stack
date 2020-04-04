1fb01b6da3eca61d0919fc644984c1db0f7a4dca
Ali Bulut <alibulut@yahoo.com>
2020-04-04 17:54:18 +0300

    users routes and controllers have been created

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -1,4 +1,6 @@
 const uuid = require('uuid/v4');
+const {validationResult} = require("express-validator");
+
 const HttpError = require("../models/http-error");
 
 let DUMMY_PLACES = [
@@ -55,6 +57,14 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty())
+    {
+        return next(new HttpError('Invalid inputs passed, please check your data!', 422))
+    }
+
     const { title, description, coordinates, address, creator } = req.body;
     const createdPlace={
         id: uuid(),
@@ -71,6 +81,13 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlaceById = (req,res,next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty())
+    {
+        return next(new HttpError('Invalid inputs passed, please check your data!', 422))
+    }
+
     const { title, description } = req.body;
     const placeId=req.params.pid;
 
@@ -88,6 +105,11 @@ const updatePlaceById = (req,res,next) => {
 
 const deletePlaceById = (req,res,next) => {
     const placeId=req.params.pid;
+    
+    if(!DUMMY_PLACES.find(p=>p.id === placeId)){
+        return next(new HttpError('Could not find a place for that id!', 404))
+    }
+
     DUMMY_PLACES=DUMMY_PLACES.filter(p => p.id!==placeId);
     res.status(200).json({message: 'The Place has been deleted!'});
 }
--- Backend/routes/users-routes.js
+++ Backend/routes/users-routes.js
@@ -1,1 +1,24 @@
- 
+const express = require("express");
+const { check } = require("express-validator");
+
+const router = express.Router();
+
+const { getUsers, signup, login } = require("../controllers/users-controller");
+
+router.get("/", getUsers);
+
+router.post(
+  "/signup",
+  [
+    check("name")
+      .not()
+      .isEmpty(),
+    check("email").isEmail().normalizeEmail(),
+    check("password").isLength({ min: 6 })
+  ],
+  signup
+);
+
+router.post("/login", login);
+
+module.exports = router;
