039ee9ada312a7600b88aaee290b339eb50a8efe
Ali Bulut <alibulut@yahoo.com>
2020-04-07 18:26:06 +0300

    updating and deleting places operations have been completed

--- Frontend/src/places/pages/UpdatePlace.js
+++ Frontend/src/places/pages/UpdatePlace.js
@@ -1,48 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import {AuthContext} from '../../shared/context/auth-context';
 
 import "./PlaceForm.css";
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Maiden Tower",
-    description: "One of the most famous place in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG",
-    address: "Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul",
-    location: {
-      lat: 41.0211216,
-      lng: 29.0041105
-    },
-    creator: "u1"
-  },
-  {
-    id: "p2",
-    title: "Maiden Tower",
-    description: "One of the most famous place in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/92/Maiden_tower.JPG",
-    address: "Salacak, Üsküdar Salacak Mevkii, 34668 Üsküdar/İstanbul",
-    location: {
-      lat: 41.0211216,
-      lng: 29.0041105
-    },
-    creator: "u2"
-  }
-];
-
 const UpdatePlace = props => {
-  const [isLoading, setIsLoading] = useState(true);
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlace, setLoadedPlace] = useState();
   const placeId = useParams().placeId;
+  const history=useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -58,33 +36,58 @@ const UpdatePlace = props => {
     false
   );
 
-  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
-
   useEffect(() => {
-    if (identifiedPlace) {
-      setFormData(
-        {
-          title: {
-            value: identifiedPlace.title,
-            isValid: true
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/${placeId}`
+        );
+        setLoadedPlace(responseData.place);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true
+            }
           },
-          description: {
-            value: identifiedPlace.description,
-            isValid: true
-          }
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifiedPlace]);
+          true
+        );
+      } catch (err) {}
+    };
+    fetchPlace();
+  }, [sendRequest, placeId, setFormData]);
 
-  const placeUpdateSubmitHandler = event => {
+  const placeUpdateSubmitHandler = async event => {
     event.preventDefault();
-    console.log(formState.inputs);
+    try {
+      await sendRequest(
+        `http://localhost:5000/api/places/${placeId}`,
+        "PATCH",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value
+        }),
+        {
+          "Content-Type": "application/json"
+        }
+      );
+      history.push(`/${auth.userId}/places`);
+    } catch (err) {}
   };
 
-  if (!identifiedPlace) {
+  if (isLoading) {
+    return (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (!loadedPlace && !error) {
     return (
       <div className="center">
         <Card>
@@ -94,41 +97,38 @@ const UpdatePlace = props => {
     );
   }
 
-  if (isLoading) {
-    return (
-      <div className="center">
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
-
   return (
-    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title!"
-        onInput={inputHandler}
-        value={formState.inputs.title.value}
-        valid={formState.inputs.title.isValid}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description! (at least 5 characters)"
-        onInput={inputHandler}
-        value={formState.inputs.description.value}
-        valid={formState.inputs.description.isValid}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        Update Place
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {!isLoading && loadedPlace && (
+        <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+          <Input
+            id="title"
+            element="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title!"
+            onInput={inputHandler}
+            value={loadedPlace.title}
+            valid={true}
+          />
+          <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description! (at least 5 characters)"
+            onInput={inputHandler}
+            value={loadedPlace.description}
+            valid={true}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            Update Place
+          </Button>
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
--- Frontend/src/shared/components/Navigation/NavLinks.js
+++ Frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,10 +1,26 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom';
 
+import { useHttpClient } from "../../hooks/http-hook";
 import {AuthContext} from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = () => {
+    const [loadedUsers, setLoadedUsers] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+          try {
+            const responseData = await sendRequest(
+              `http://localhost:5000/api/users`
+            );
+            setLoadedUsers(responseData.users);
+          } catch (err) {}
+        };
+        fetchUsers();
+      }, [sendRequest]);
+    
     const auth = useContext(AuthContext);
     return (
             <ul className="nav-links">
@@ -12,10 +28,13 @@ const NavLinks = () => {
                     <NavLink to="/" exact>All Users</NavLink>
                 </li>
                 {auth.isLoggedIn && <li style={{marginRight: 25 + 'px'}}>
-                    <NavLink to="/u1/places">My Places</NavLink>
+                    <NavLink to={`/${auth.userId}/places`}>My Places</NavLink>
                 </li>}
                 {auth.isLoggedIn &&<li style={{marginRight: 25 + 'px'}}>
                     <NavLink to="/places/new">Add Place</NavLink>
+                </li>}
+                {auth.isLoggedIn &&<li style={{marginRight: 25 + 'px'}}>
+                    <span>Current User : {loadedUsers.filter(user => user.id === auth.userId).map(user => user.name)}</span>
                 </li>}
                 {!auth.isLoggedIn &&<li style={{marginRight: 25 + 'px'}}>
                     <NavLink to="/auth">Authenticate</NavLink>
