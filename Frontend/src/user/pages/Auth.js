8a96162d8ca478b8fd9db8388a52cd77f42fec7d
Ali Bulut <alibulut@yahoo.com>
2020-04-08 15:44:12 +0300

    image upload operations have been done for frontend side

--- Frontend/src/user/pages/Auth.js
+++ Frontend/src/user/pages/Auth.js
@@ -6,6 +6,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -40,7 +41,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -51,6 +53,10 @@ const Auth = () => {
           name: {
             value: "",
             isValid: false
+          },
+          image:{
+            value:null,
+            isValid:false
           }
         },
         false
@@ -62,6 +68,7 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+    console.log(formState.inputs);
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -113,6 +120,8 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {/* we use inputHandler because it's nearly same with our expectation */}
+          {!isLoginMode && <ImageUpload id="image" center onInput={inputHandler} />}
           <Input
             element="input"
             id="email"
